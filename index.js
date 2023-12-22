884dd41d58a994ecd96c84a0b1bf0858dcee9a10
rayhan9802 <mdrayhaanahmed@gmail.com>
2023-12-22 15:39:13 +0600

    post data from database

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const taskCollection = client.db('TaskDB').collection('tasks')
+
+    // task collection
+    app.post('/task', async(req, res) => {
+        const body = req.body;
+        const result = await taskCollection.insertOne(body)
+        res.send(result)
+    })
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
